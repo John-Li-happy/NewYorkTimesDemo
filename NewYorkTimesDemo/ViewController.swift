dfa6d7e4baac2e2ef13368c2eda497328e868f95
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2020-10-30 17:01:39 -0400

    Initial Commit

---
+++ NewYorkTimesDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewYorkTimesDemo
+//
+//  Created by Zhaoyang Li on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
